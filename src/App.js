c4911103dd3f3d6a35187a14c404ee64a964cd6a
jordi3d <jordi@cine3d.com>
2022-02-24 19:11:22 +0100

    posant ordre

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "./App.css";
-
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { counter, increment, reset } from "./counter";
+import { counter } from "./counter";
 import CounterRedux from "./CounterRedux";
 import CounterUserReducer from "./CounterUserReducer";
 
 const store = createStore(counter, 0);
-store.dispatch(increment(+1));
-store.dispatch(increment(+1));
-store.dispatch(increment(+1));
-store.dispatch(increment(+1));
-console.log("state:", store.getState());
-store.dispatch(increment(+1));
-console.log("state:", store.getState());
-store.dispatch(reset());
-console.log("state:", store.getState());
-store.dispatch(increment(-1));
-console.log("state:", store.getState());
 
 function App() {
   return (
